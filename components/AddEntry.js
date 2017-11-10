ddf4ec60148a1a381847f1fb39f7c0ddf2029397
mostafa69d <mostafa69d@gmail.com>
2017-11-10 17:37:00 -0500

    style: add style to stepper slider and the whole add entry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -135,6 +135,7 @@ const styles=StyleSheet.create({
     container:{
         flex:1,
         padding:20,
+        paddingTop:50,
         backgroundColor:white
     },
     row:{
--- components/DateHeader.js
+++ components/DateHeader.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {Text} from 'react-native'
-
+import {purple} from '../utils/colors'
 export default function DateHeader({date}){
     return(
-        <Text>{date}</Text>
+        <Text style={{color:purple,fontSize:35,marginBottom:10}}>{date}</Text>
     )
 }
