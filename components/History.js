31e6ebc36b90595ac6d96357a984d82d3ef257ce
mostafa69d <mostafa69d@gmail.com>
2017-11-16 10:22:54 -0500

    add navigation tab

--- components/History.js
+++ components/History.js
@@ -8,7 +8,12 @@ import UdaciFitnessCalendar from 'udacifitness-calendar'
 import { white } from '../utils/colors'
 import DateHeader from './DateHeader'
 import MetricCard from './MetricCard'
+import {AppLoading} from 'expo'
+
 class History extends Component {
+    state={
+        ready:false
+    }
     componentDidMount() {
         const { dispatch } = this.props
 
@@ -22,6 +27,7 @@ class History extends Component {
                 }
             })
             .then(() => this.setState(() => ({ ready: true })))
+            .then(() =>this.setState({ready:true}))
     }
     renderItem = ({ today, ...metrics }, formattedDate, key) => (
         <View style={styles.item}>
@@ -51,6 +57,13 @@ class History extends Component {
     }
     render() {
         const { entries } = this.props
+        const {ready}=this.state
+
+        if(ready===false){
+            return(
+                <AppLoading/>
+            )
+        }
         return (
             <UdaciFitnessCalendar
                 items={entries}
